3ac5d2a1d2adff291cef12965ce7f08645939fc0
Home\Steffen <stef.holt@yahoo.de>
2018-01-28 00:46:55 +0100

    Add component for article intro with all current informations

---
+++ src/components/overview/articleIntro/ArticleIntro.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {TextWithIcon} from '../../utils/textWithIcon/TextWithIcon'
+import ArticleInformation from './articleInformation/ArticleInformation'
+import './article-intro.scss';
+
+
+export default class ArticleIntro extends Component {
+
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <div className='content__card'>
+                <div className='intro-wrapper'>
+                    <div className='description'>
+                        <h1 className ='name'> Big BBQ Burger</h1>
+                        <p className = 'text'>Rinderhacksteak vom Grill im Sesambrötchen mit Blattsalat, Tomate, Gurke, Zwiebeln, Käse und BBQ-Sauc'e. Dazu gibt es Cajuns. </p>
+                    </div>
+                    <div className='image'>
+                        <img src='https://tsimg.space/v1/images/c978fada-65db-e711-8381-00155d099e09.jpg'></img>
+                    </div>
+                </div>    
+
+                <ArticleInformation/>
+            </div>
+        );
+    }
+}
+/*
+                        <div style={{backgroundImage: "url('https://tsimg.space/v1/images/c978fada-65db-e711-8381-00155d099e09.jpg')"}}></div>                        
+
+            `*/
