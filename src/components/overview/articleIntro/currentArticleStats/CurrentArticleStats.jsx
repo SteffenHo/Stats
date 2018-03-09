f992401796fd6a89832eefc6aee2bf029ec24e0d
Home\Steffen <stef.holt@yahoo.de>
2018-03-09 19:21:00 +0100

    Change use new stats

--- src/components/overview/articleIntro/currentArticleStats/CurrentArticleStats.jsx
+++ src/components/overview/articleIntro/currentArticleStats/CurrentArticleStats.jsx
@@ -3,6 +3,7 @@ import BarChart from '../../statistic/barChart/BarChart';
 import DonutChart from '../../statistic/donutChart/DonutChart'
 import GaugeChart from '../../statistic/gaugeChart/GaugeChart'
 import ChartAccordion from '../../statistic/chartAccordion/ChartAccordion';
+import VerticleBarChart from '../../statistic/verticleBarChart/VerticleBarChart'
 import './current-article-stats.scss'
 
 
@@ -17,6 +18,15 @@ export default class CurrentArticleStats extends Component {
         const gaugeColumns = [
             ['Umsatz', 91.4]
         ];
+
+        const turnOverColumns = [
+            ['x', 'Heute', 'Gestern'],
+            ['Absatz', 500, 632]
+        ]
+        const salesColumns = [
+            ['x', 'Heute', 'Gestern'],
+            ['Umsatz', 50000, 63200]
+        ]
 
         const salesGaugeColumns = [
             ['Absatz', 73.2]
@@ -34,11 +44,11 @@ export default class CurrentArticleStats extends Component {
                     <div className="stats-wrapper">
                         <div className="stats-description">
                             <div> Hier wird dein Tagesumsatz mit den des Vortags verglichen, bisher hast du <span className="bold">91,4%</span> des Vortagsumsatzes gemacht.</div>
-                            <GaugeChart columns={gaugeColumns} width={200} />
+                            <VerticleBarChart columns={salesColumns} width={200} height={150} tooltipValueFormat={(value, ratio, id) => { return `${value}€`;}}/>
                         </div>
                         <div className="stats-description">
-                            <div> Heute hast du schon <span className="bold">302 Big BBQ Burger</span> verkauft, dir fehlen noch <span className='bold'>26,8%</span>, um das Ziel von gestern zu erreichen</div>
-                            <GaugeChart columns={salesGaugeColumns} width={200} />
+                            <div> Heute hast du schon <span className="bold">500 Big BBQ Burger</span> verkauft, dir fehlen noch <span className='bold'>6,8%</span>, um das Ziel von gestern zu erreichen</div>
+                            <VerticleBarChart columns={turnOverColumns} width={200} height={150} tooltipValueFormat={(value, ratio, id) => {return `${value} Stück`;}}/>
                         </div>
 
                     </div>
@@ -53,3 +63,6 @@ export default class CurrentArticleStats extends Component {
         );
     }
 }
+
+// <GaugeChart columns={gaugeColumns} width={200} />
+// <GaugeChart columns={salesGaugeColumns} width={200} />
