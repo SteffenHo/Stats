093bb011bd837721d605fc371b19d3adb9386319
Home\Steffen <stef.holt@yahoo.de>
2018-03-09 19:20:29 +0100

    Add component for verticle  bar chart to show differences

---
+++ src/components/overview/statistic/verticleBarChart/VerticleBarChart.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './verticle-bar-chart.scss';
+import { Accordion } from 'chayns-components';
+import PropTypes from 'prop-types';
+import C3Chart from 'react-c3js';
+
+export default class VerticleBarChart extends Component {
+
+    constructor() {
+        super();
+       
+    }
+
+    static propTypes = {
+        columns: PropTypes.array.isRequired,
+        tooltipTitleFormat: PropTypes.func,
+        tooltipValueFormat: PropTypes.func,
+        xAxisFormat: PropTypes.fun,
+        duration:PropTypes.number,
+    }
+
+    static defaultProps = {
+        duration: 1000,
+        tooltipNameFormat: (name, ratio, id, index) => {console.log('name, ratio, id, index',name, ratio, id, index); return name},
+        tooltipTitleFormat: function (d) { console.log('data', d ); },
+        tooltipValueFormat: function (value, ratio, id, index) { console.log('value, ratio, id', value, ratio, id, index); return `${value}`},
+        //xAxisFormat: function (d) { return `${d.getDate()}.${d.getMonth()}` }
+        yAxisFormat: (d)=>{console.log(d); return d.toFixed(0)}
+    }
+    
+    
+    render() {
+        const {columns, duration, tooltipTitleFormat, tooltipValueFormat,xAxisFormat, width, height, yAxisFormat, tooltipNameFormat} = this.props;
+        const data = {
+            x: 'x',
+            columns,
+            type: 'bar',
+          };
+        const axis ={
+            rotated: true,
+            x : {
+                    type : 'category',
+                },
+            y: {
+                tick: {
+                        count: 3,
+                        format: yAxisFormat,
+                        outer: false
+                    }
+                }
+        }
+        const transition = {
+            duration
+        }
+        const tooltip = {
+            format: {
+                name: tooltipNameFormat,
+                title: tooltipTitleFormat,
+                value: tooltipValueFormat
+            }
+        }
+        const size = {
+            width,
+            height
+        }
+        const padding = {
+            right: 20
+        }
+        return (
+            <C3Chart data={data} axis={axis} transition={transition} tooltip={tooltip} size={size} padding={padding}/>
+        );
+    }
+}
